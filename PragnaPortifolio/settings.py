5df7dbc55054143712a3439f65f0c50944c6f411
Pragna <pragnapriyathamkandula@my.unt.edu>
2025-02-08 10:21:12 -0600

    settings

--- PragnaPortifolio/settings.py
+++ PragnaPortifolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-85t%)xy5tujn&^s3t2(9$c261e^%o)_9d(sk0d78ku#i^vxhj0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -75,8 +75,6 @@ WSGI_APPLICATION = 'PragnaPortifolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
-import os
 
 if os.getenv("DOCKERIZED"):  # Check if running in Docker
     DATABASES = {
